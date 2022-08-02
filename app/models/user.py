4bfd527e741a93e6ff47f7697cd1a2596bd92fb2
Moe <moe62197@gmail.com>
2022-08-02 11:45:09 -0400

    initiating database

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,18 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
+    bio = db.Column(db.String(255))
+    profileImg = db.Column(db.String(255))
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    birthday = db.Column(db.String(35), nullable=True)
+    location = db.Column(db.String(50), nullable=True)
+    joined = db.Column(db.DateTime)
+
+    creets = db.relationship("Creet", back_populates="users")
+    comments = db.relationship("Comment", back_populates="users")
 
     @property
     def password(self):
@@ -26,5 +35,12 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'name': self.name,
+            'bio': self.bio,
+            'profileImg': self.profileImg,
+            'email': self.email,
+            'hashed_password': self.hashed_password,
+            'birthday': self.birthday,
+            'location': self.location,
+            "joined": self.joined
         }
