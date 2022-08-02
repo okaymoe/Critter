84296bae4ec5f16d7544f5c7e33e3902a89152fd
Moe <moe62197@gmail.com>
2022-08-02 15:49:21 -0400

    redo heroku

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,7 +4,7 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='demopuppy', name="Demo Puppy", bio='Just taking a sniff of this website...I like it!', profile_img='https://pbs.twimg.com/profile_images/1552995729014247425/TaJbIdmK_400x400.jpg', header_img='https://www.allandetrobert.com/wp-content/uploads/2017/09/header-pet-food.jpg', email='demo@aa.io', password='password', birthday='March 13th, 1995', joined='2019-03-17 12:12:22')
+        username='demopuppy', name="Demo Puppy", bio='Just taking a sniff of this website...I like it!', profile_img='https://pbs.twimg.com/profile_images/1552995729014247425/TaJbIdmK_400x400.jpg', header_img='https://www.allandetrobert.com/wp-content/uploads/2017/09/header-pet-food.jpg', email='demo@aa.io', password='password', birthday='March 12th, 1995', joined='2019-03-17 12:12:22')
     marnie = User(
         username='marnie', name="Marnie the Hamster", bio='Where da nuts at', profile_img='https://m.facebook.com/1083131885037198/photos/a.1083135208370199/1367022526648131/?type=3&source=48&__tn__=EH-R', header_img='https://pbs.twimg.com/media/EwmFxf8UUAExODa.jpg', email='marnie@aa.io', password='password', birthday='August 1st, 1993', joined='2011-11-06 12:12:22')
     bobbie = User(
