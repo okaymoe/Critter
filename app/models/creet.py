6d92c6ae0426566dbf8fb74c57121c603d28a959
Moe <moe62197@gmail.com>
2022-08-02 12:11:12 -0400

    database done for users comments and creets

---
+++ app/models/creet.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from .db import db
+
+class Creet(db.Model):
+    __tablename__ = 'creets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    image_url = db.Column(db.String(), nullable=True)
+    content = db.Column(db.String(280))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    # Relationships
+    users = db.relationship("User", back_populates="creets")
+    comments = db.relationship("Comment", back_populates="creets", cascade="all, delete")
+
+
+
+    # Grab all
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_url": self.image_url,
+            "content": self.content,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from .db import db
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -10,12 +11,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(30), nullable=False, unique=True)
     name = db.Column(db.String(50), nullable=False)
     bio = db.Column(db.String(255))
-    profileImg = db.Column(db.String(255))
+    profile_img = db.Column(db.String(255))
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     birthday = db.Column(db.String(35), nullable=True)
     location = db.Column(db.String(50), nullable=True)
     joined = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
 
     creets = db.relationship("Creet", back_populates="users")
     comments = db.relationship("Comment", back_populates="users")
@@ -37,10 +40,12 @@ class User(db.Model, UserMixin):
             'username': self.username,
             'name': self.name,
             'bio': self.bio,
-            'profileImg': self.profileImg,
+            'profile_img': self.profile_img,
             'email': self.email,
             'hashed_password': self.hashed_password,
             'birthday': self.birthday,
             'location': self.location,
-            "joined": self.joined
+            "joined": self.joined,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
         }
