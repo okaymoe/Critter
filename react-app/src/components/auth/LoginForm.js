66fe5beac1ae1f02b975ae56f7a446b5cbba8091
Moe <moe62197@gmail.com>
2022-08-23 05:39:27 -0400

    login

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import './LoginForm.css';
 
-
-const LoginForm = () => {
+const LoginForm = ({ setShowModal }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -32,34 +33,54 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='LoginWrapper'>
+      <div className='LoginClose'>
+        <div onClick={() => setShowModal(false)} className='LoginCloseButtonContainer'>
+          <HighlightOffIcon className='LoginCloseButton' />
+        </div>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
+      <div className='Loginwithoutclose'>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous"
+      />
+        <i class="fas fa-paw" className="mylogo"></i>
+        <p className='LoginText'>Sign in to Critter</p>
+
+        <form onSubmit={onLogin}>
+        <div className='LoginErrorContainer'>
+          {errors.map((error, ind) => (
+            <div className='LoginError' key={ind}>{error}</div>
+          ))}
+        </div>
+          <div className='LoginFieldContainer'>
+            <label className='LoginLabel' htmlFor='email'>Email</label>
+            <input
+              className='LoginInput'
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className='LoginFieldContainer'>
+            <label className='LoginLabel' htmlFor='password'>Password</label>
+            <input
+              className='LoginInput'
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+        </form>
+          <button onClick={onLogin} className='LoginSubmit' type='submit'>Sign in</button>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
